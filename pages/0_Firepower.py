88ab02bc3252ca1856723627667bce3bff6f0029
LachinFernando <106962694+LachinFernando@users.noreply.github.com>
2024-03-22 04:36:57 +0000

    add(components): pages and geopandas artifiacts

---
+++ pages/0_Firepower.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+
+
+# constants
+DATE_ONE = "2014-03-20"
+DATE_TWO = "2016-08-31"
+
+
+# functions
+def create_plotly_map(date: str) -> None:
+    file_name = date + "_geodata.gpkg"
+    # csv_filename = date + "_final_merged.csv"
+    # csv = pd.read_csv(csv_filename)
+    merged_geo_data = gpd.read_file(file_name)
+
+    fig = px.choropleth(merged_geo_data, geojson=merged_geo_data.geometry, locations= merged_geo_data.index, color=merged_geo_data.frp,
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 1),
+                           scope="usa",
+                           labels={'frp':'FirePower'}
+                          )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}) 
+
+    # set the plot
+    # heading
+    st.subheader(f"Firepower on {date} across USA")
+    st.plotly_chart(fig)
+
+# set title
+st.title("Interactive Visualization: Firepower")
+
+# set a dropdown
+date = st.selectbox(
+    "Please choose a date you would like to test...",
+    (DATE_ONE, DATE_TWO),
+    placeholder = "Please select a date"
+)
+
+if date == DATE_ONE:
+    create_plotly_map(date)
+elif date == DATE_TWO:
+    create_plotly_map(date)
+
+
+
